44aa40cd1108da3bf8a4ac51261f6ce03f18080b
TaIvAn0913MBD <24HP6298@nest.edu.mn>
2024-10-29 19:15:00 +0800

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+// const og = [2, 4, 6, 8, 10, 1, 5, 9];
+// // const text = document.getElementById("text");
+// // const nnumbers = og.map(myFunction);
+// // function myFunction(num) {
+// //   return num * 2;
+// // }
+
+// // const nnumbers = og.filter(myFunction);
+// // function myFunction(num) {
+// //   if (num % 2 == 1) {
+// //     return num;
+// //   }
+// // }
+// // text.innerText = nnumbers;
+
+// // const nnumbers = [
+// //   { one: 1, two: 1 },
+// //   { one: 2, two: 2 },
+// //   { one: 3, two: 3 },
+// //   { one: 4, two: 4 },
+// // ];
+// // const sum = nnumbers.map(myFunction);
+// // function myFunction(too) {
+// //   return " " + too.one * too.two + " ";
+// // }
+// // text.innerText = sum;
+// // let count = 0;
+// const users = [
+//   { names: "Bob", age: 25 },
+//   { names: "Steve", age: 23 },
+//   { names: "Alex", age: 37 },
+// ];
+// const sum = users.map(myFunction);
+
+// // function myFunction(nas) {
+// //   count += nas.age;
+// // }
+// // text.innerText = Math.round(count / users.length);
+// function myFunction(ner) {
+//   return ner.names;
+// }
+// console.log(sum);
